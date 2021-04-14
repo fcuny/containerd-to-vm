cfdef4995afd80962dede1a7bf8b4431b3a2c7d0
Franck Cuny <franck.cuny@gmail.com>
2021-04-13 19:41:04 -0700

    pull a container into a namespace
    Pull an image provided as an argument to the program. We're only
    interested in images for Linux/amd64 at the moment.
    
    We setup a default namespace for containerd named `c2vm`. Images that
    will be pulled by containerd will be stored inside that namespace.
    
    Once the program is build it can be run like this:
    ```
    ; sudo ./c2vm -container docker.io/library/redis:latest
    pulled docker.io/library/redis:latest (38667897 bytes)
    ```
    
    And the image is indeed in the namespace:
    ```
    ; sudo ctr -n c2vm images ls -q
    docker.io/library/redis:latest
    ```

---
+++ cmd/c2vm/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/containerd/containerd"
+	"github.com/containerd/containerd/namespaces"
+	"github.com/containerd/containerd/platforms"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+)
+
+const (
+	containerdSock   = "/run/containerd/containerd.sock"
+	defaultNamespace = "c2vm"
+)
+
+var (
+	platform = platforms.Only(ocispec.Platform{
+		OS:           "linux",
+		Architecture: "amd64",
+	})
+)
+
+func main() {
+	var (
+		containerName = flag.String("container", "", "Name of the container")
+	)
+
+	flag.Parse()
+
+	if *containerName == "" {
+		log.Fatal("a container is required")
+	}
+
+	client, err := containerd.New(containerdSock)
+	if err != nil {
+		log.Fatalf("failed to create a client for containerd: %v", err)
+	}
+	defer client.Close()
+
+	ctx := namespaces.WithNamespace(context.Background(), defaultNamespace)
+	image, err := client.Pull(ctx, *containerName, containerd.WithPlatformMatcher(platform))
+	if err != nil {
+		log.Fatalf("failed to pull the container %s: %v\n", *containerName, err)
+	}
+
+	imageSize, err := image.Usage(ctx, containerd.WithUsageManifestLimit(1))
+	if err != nil {
+		log.Fatalf("failed to get the size of the image: %v", err)
+	}
+
+	fmt.Printf("pulled %s (%d bytes)\n", image.Name(), imageSize)
+}
---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/fcuny/containerd-to-vm
+
+go 1.16
+
+require (
+	github.com/Microsoft/go-winio v0.4.17 // indirect
+	github.com/Microsoft/hcsshim v0.8.16 // indirect
+	github.com/containerd/containerd v1.5.0-beta.4
+	github.com/containerd/continuity v0.0.0-20210315143101-93e15499afd5 // indirect
+	github.com/containerd/fifo v0.0.0-20210331061852-650e8a8a179d // indirect
+	github.com/containerd/typeurl v1.0.2 // indirect
+	github.com/gogo/googleapis v1.4.1 // indirect
+	github.com/google/go-cmp v0.5.5 // indirect
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/opencontainers/image-spec v1.0.1
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/net v0.0.0-20210410081132-afb366fc7cd1 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20210412220455-f1c623a9e750 // indirect
+	google.golang.org/grpc v1.37.0 // indirect
+)
